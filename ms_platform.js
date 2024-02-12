5217170dbc5c58eb7b960c5348daa9579211f43d
htsd209 <144764151+htsd209@users.noreply.github.com>
2024-02-12 11:17:22 +0800

    Update ms_platform.js

--- ms_platform.js
+++ ms_platform.js
@@ -10,10 +10,10 @@ if (queryString) {
             if(decodeURIComponent(paramNameValuePair[0]) == 'ms_id'){
                 console.log(decodeURIComponent(paramNameValuePair[0]));
                 console.log(decodeURIComponent(paramNameValuePair[0]));
-                playerthe.innerHTML = "<audio controls='' id='player' src='audios/"+decodeURIComponent(paramNameValuePair[1])+".mp3'><\/audio>";
+                document.getElementById('playerthe').innerHTML = "<audio controls='' id='player' src='audios/"+decodeURIComponent(paramNameValuePair[1])+".mp3'><\/audio>";
                 console.log('music:played');
                 console.log("<audio controls='' id='player' src='audios/"+decodeURIComponent(paramNameValuePair[1])+".mp3'><\/audio>");
-                tit.innerHTML = msic[decodeURIComponent(paramNameValuePair[1])];
+                document.getElementById('tit').innerHTML = msic[decodeURIComponent(paramNameValuePair[1])];
                 console.log("music:title showed!");
             }
             else{
